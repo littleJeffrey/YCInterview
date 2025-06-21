0eae07ce1c84ed81a40e8e76e968ee288a3bf731
littleJeffrey <byby88323@gmail.com>
2025-06-21 14:42:25 +0800

    完成Index、Create、Edit和Delete的Controller功能和View UI。

--- YCInterview/Controllers/TodoController.cs
+++ YCInterview/Controllers/TodoController.cs
@@ -1,26 +1,96 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 using YCInterview.Models;
 
 namespace YCInterview.Controllers
 {
     public class TodoController : Controller
     {
-        private readonly ILogger<TodoController> _logger;
+        private readonly TodoDbContext _context;
 
-        public TodoController(ILogger<TodoController> logger)
+        public TodoController(TodoDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var items = _context.TodoItems.ToList();
+            return View(items);
+        }
+        public IActionResult Create()
+        {
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(TodoItem item)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                _context.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(item);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var item = await _context.TodoItems.FindAsync(id);
+            if (item == null) return NotFound();
+
+            return View(item);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, TodoItem item)
+        {
+            if (id != item.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(item);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.TodoItems.Any(e => e.Id == id))
+                        return NotFound();
+                    else
+                        throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(item);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var item = await _context.TodoItems
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (item == null) return NotFound();
+
+            return View(item);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var item = await _context.TodoItems.FindAsync(id);
+            _context.TodoItems.Remove(item);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
