af7c95c15afb5916b079a3b9b122b7861a48c83c
littleJeffrey <byby88323@gmail.com>
2025-06-21 14:15:31 +0800

    加入專案檔案。

---
+++ YCInterview/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YCInterview.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
